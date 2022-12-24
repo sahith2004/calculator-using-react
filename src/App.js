a7f513303b0e5f696f3a3d1352902e26ed834b78
sahith <shanmukhasahith@gmail.com>
2022-12-24 21:45:24 +0530

    this is the first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [input,setInput] = useState('');
+  const [display,setDisplay]= useState(0);
+  function clickHandler(char){
+      setInput(input+char)
+  }
+  const getAnswer = (e)=>{
+      setInput(e.target.value)
+      
+  }
+  const displayAnswer = ()=>{
+      
+      setDisplay(
+      eval(input))
+
+  }
+  const clearInput = ()=>{
+    setInput('')
+    setDisplay(0)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     
+    <div className='main-calculator'>
+        <input value={input} name="input" type="text" onChange={getAnswer}></input>
+        <h2>The result is {display}</h2>
+       
+          <div className='main-button'>
+            <button className='btn' onClick={()=>{setInput(input+'1')}}>1</button>
+            <button className='btn' onClick={()=>{setInput(input+'2')}}>2</button>
+            <button className='btn' onClick={()=>{setInput(input+'3')}}>3</button>
+            <button className='btn' onClick={()=>{setInput(input+'4')}}>4</button>
+            <button className='btn' onClick={()=>{setInput(input+'5')}}>5</button>
+            <button className='btn' onClick={()=>{setInput(input+'6')}}>6</button>
+            <button className='btn' onClick={()=>{setInput(input+'7')}}>7</button>
+            <button className='btn' onClick={()=>{setInput(input+'8')}}>8</button>
+            <button className='btn' onClick={()=>{setInput(input+'9')}}>9</button>
+            <button className='btn' onClick={()=>{setInput(input+'0')}}>0</button>
+            <button className='btn' onClick={()=>{setInput(input+'+')}}>+</button>
+            <button className='btn' onClick={()=>{setInput(input+'-')}}>-</button>
+            <button className='btn' onClick={()=>{setInput(input+'*')}}>X</button>
+            <button className='btn' onClick={()=>{setInput(input+'/')}}>/</button>
+            <button className='btn btn-n' onClick={displayAnswer}>=</button>
+          <button className='btn btn -n'  onClick={clearInput }>C</button>
+          </div>
+         
+
+      </div>
+  
+    
+   
+    </>
   );
 }
 
